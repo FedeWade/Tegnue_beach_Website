06a21834c3fc589132290cfc9de10a8d783c6113
Federico Ballarin <62607753+FedeWade@users.noreply.github.com>
2022-04-23 13:52:12 +0200

    Add files via upload

---
+++ TegnueTecWeb1819/php/connection.php
@@ -0,0 +1,49 @@
+<?php
+class DBConnection
+{
+	const host = 'localhost';
+	const user = 'root';
+	const pwd = '';
+	const db = 'tegnue';
+
+	public $connectionOpen = false;
+	public $failedConnection = false;
+	public $connection;
+
+	public function openConnection()
+	{
+		if ($this->failedConnection)
+		return false;
+		$this->connection = @mysqli_connect(static::host, static::user, static::pwd, static::db);
+		
+		mysqli_query($this->connection,"SET NAMES 'utf8'");
+		if (!$this->connection) 
+		{
+		$this->failedConnection = true;
+		return false;
+		}
+    $this->connectionOpen = true;
+    return true;
+	}
+  
+	private function getAllQuery($query)
+	{
+		$result = mysqli_query($this->connection, $query);
+		return mysqli_fetch_all($result, MYSQLI_ASSOC);
+	}
+
+	public function getelements($group)
+	{
+		return $this->getAllQuery("SELECT ElementoMenu.Nome AS nomepiatto, ElementoMenu.Prezzo AS prezzopiatto, Gruppo.Nome AS nomegruppo 
+								   FROM ElementoMenu JOIN Gruppo ON ElementoMenu.Gruppo=Gruppo.CodiceGruppo 
+								   WHERE Gruppo.Nome='$group' ");
+	}
+	
+	public function closeConnection()
+	{
+		if ($this->connectionOpen)
+			mysqli_close($this->connection);
+		$this->connectionOpen = false;
+	}
+}
+?>
